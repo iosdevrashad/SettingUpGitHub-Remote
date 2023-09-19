abcba03f483ecca9f5a1dc6eb23c2560aa055932
iosdevrashad <rashad8787@me.com>
2023-09-18 20:45:05 -0700

    SecondView Added

---
+++ SettingUpGitHub/ViewTwo.swift
@@ -0,0 +1,20 @@
+//
+//  ViewTwo.swift
+//  SettingUpGitHub
+//
+//  Created by Rashad Surratt on 9/18/23.
+//
+
+import SwiftUI
+
+struct ViewTwo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewTwo_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewTwo()
+    }
+}
