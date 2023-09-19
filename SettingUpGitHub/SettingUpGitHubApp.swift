d61d984e63afcf23376b34c9fe6d34ff94e474be
iosdevrashad <rashad8787@me.com>
2023-09-18 20:34:41 -0700

    Initial Commit

---
+++ SettingUpGitHub/SettingUpGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingUpGitHubApp.swift
+//  SettingUpGitHub
+//
+//  Created by Rashad Surratt on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SettingUpGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
